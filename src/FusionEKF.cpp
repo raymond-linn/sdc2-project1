85ea233096e794c32ee0ad5cad5ea82e6f584e51
Raymond Linn <rlinn@alpine-la.com>
2017-03-15 09:07:44 -0700

    first commit

---
+++ src/FusionEKF.cpp
@@ -0,0 +1,147 @@
+#include "FusionEKF.h"
+#include <iostream>
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::vector;
+
+/*
+ * Constructor.
+ */
+FusionEKF::FusionEKF() {
+  is_initialized_ = false;
+
+  previous_timestamp_ = 0;
+
+  // initializing matrices
+  R_laser_ = MatrixXd(2, 2);
+  R_laser_ << 0.0225, 0,
+              0, 0.0225;
+  R_radar_ = MatrixXd(3, 3);
+  R_radar_ << 0.0225, 0, 0,
+              0, 0.0225, 0,
+              0, 0, 0.0225;
+
+  H_laser_ = MatrixXd(2, 4);
+  H_laser_ << 1, 0, 0, 0,
+              0, 1, 0, 0;
+
+  VectorXd x_ = VectorXd(4);
+  MatrixXd F_ = MatrixXd(4, 4);
+  F_ << 1, 0, 1, 0,
+          0, 1, 0, 1,
+          0, 0, 1, 0,
+          0, 0, 0, 1;
+  MatrixXd Q_ = MatrixXd(4, 4);
+  MatrixXd P_ = MatrixXd(4, 4);
+  P_ << 1, 0, 0, 0,
+          0, 1, 0, 0,
+          0, 0, 1000, 0,
+          0, 0, 0, 1000;
+  ekf_.Init(x_, P_, F_, H_laser_, R_laser_, Q_);
+
+  Hj_ = MatrixXd(3, 4);
+
+  noise_ax = 9;
+  noise_ay = 9;
+}
+
+/**
+* Destructor.
+*/
+FusionEKF::~FusionEKF() {}
+
+void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
+  /*****************************************************************************
+   *  Initialization
+   ****************************************************************************/
+  if (!is_initialized_) {
+    float px = 0;
+    float py = 0;
+    float vx = 0;
+    float vy = 0;
+
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      /**
+      Convert radar from polar to cartesian coordinates and initialize state.
+      */
+      float ro = measurement_pack.raw_measurements_[0];
+      float phi = measurement_pack.raw_measurements_[1];
+      float ro_dot = measurement_pack.raw_measurements_[2];
+      px = ro * cos(phi);
+      py = ro * sin(phi);
+      vx = ro_dot * cos(phi);
+      vy = ro_dot * sin(phi);
+    }
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+      /**
+      Initialize state.
+      */
+      px = measurement_pack.raw_measurements_[0];
+      py = measurement_pack.raw_measurements_[1];
+      vx = 0;
+      vy = 0;
+
+    }
+
+    ekf_.x_ << px, py, vx, vy;
+    previous_timestamp_ = measurement_pack.timestamp_;
+
+    // done initializing, no need to predict or update
+    is_initialized_ = true;
+    return;
+  }
+
+  /*****************************************************************************
+   *  Prediction
+   ****************************************************************************/
+  double dt = (measurement_pack.timestamp_ - previous_timestamp_)/1000000.0;
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  double dt_2 = dt * dt;
+  double dt_3 = dt_2 * dt;
+  double dt_4 = dt_3 * dt;
+  ekf_.F_ << 1, 0, dt, 0,
+              0, 1, 0, dt,
+              0, 0, 1, 0,
+              0, 0, 0, 1;
+
+  ekf_.Q_ << dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+              0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+              dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+              0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
+
+  ekf_.Predict();
+
+  /*****************************************************************************
+   *  Update
+   ****************************************************************************/
+
+  /**
+   TODO:
+     * Use the sensor type to perform the update step.
+     * Update the state and covariance matrices.
+   */
+
+  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+    // Radar updates
+    try {
+      ekf_.R_ = R_radar_;
+      Hj_ = tools.CalculateJacobian(ekf_.x_);
+      ekf_.H_ = Hj_;
+      ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+    }catch (...){
+      // exception from Jacobian calculation
+      // don't do anything for the case where dividing with less than 0.0001
+    }
+  } else {
+    // Laser updates
+    ekf_.R_ = R_laser_;
+    ekf_.H_ = H_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
+  }
+  // print the output
+  //cout << "x_ = " << ekf_.x_ << endl;
+  //cout << "P_ = " << ekf_.P_ << endl;
+}
---
+++ src/tools.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using namespace std;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+
+    // check the validity of the following inputs:
+    //  * the estimation vector size should not be zero
+    //  * the estimation vector size should equal ground truth vector size
+    // ... your code here
+    if(estimations.size() == 0 | estimations.size() != ground_truth.size()){
+        cout << "either estimation size is 0 or estimation and ground truth are not the same size" << endl;
+        return rmse;
+    }
+
+    //accumulate squared residuals
+    for(int i=0; i < estimations.size(); ++i){
+        // ... your code here
+        VectorXd residual = estimations[i] - ground_truth[i];
+        // coefficientwise multiplication
+        residual = residual.array()*residual.array();
+        rmse += residual;
+    }
+
+    //calculate the mean
+    // ... your code here
+    rmse = rmse/estimations.size();
+
+    //calculate the squared root
+    // ... your code here
+    rmse = rmse.array().sqrt();
+
+    //return the result
+    return rmse;
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+
+    MatrixXd Hj(3,4);
+    //recover state parameters
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+
+    float c1 = px*px + py*py;
+    float c2 = sqrt(c1);
+    float c3 = (c1*c2);
+
+    //check division by zero
+    if (fabs(c1) < 0.0001) {
+        cout << "Cannot be divided by zero" << endl;
+        // instead of returning with non-initialized Hj throw the exception for
+        // the caller function to catch the exception
+        // return Hj;
+        throw 1;
+    }
+
+    //compute the Jacobian matrix (3x4)
+    Hj << (px/c2), (py/c2), 0, 0,
+            -(px/c1), (px/c1), 0, 0,
+            py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+
+    return Hj;
+}
